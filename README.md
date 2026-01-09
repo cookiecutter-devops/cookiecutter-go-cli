7a63d1e2b660d037ffb60f8a313e2cab3fe9d0d8
hujianli94 <1879324764@qq.com>
2026-01-09 14:50:02 +0800

    add code

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # cookiecutter-go-cli
 
+一个使用 cookiecutter 生成的 Go 命令行工具项目的模板。
+
+使用三方库：
+
+- cobra
+- viper
+- zerolog
+
+
 ## 使用
 
 ```sh
@@ -13,4 +22,98 @@ cookiecutter gh:cookiecutter-devops/cookiecutter-go-cli
   [7/9] git (y): y
   [8/9] subcommands (n): show
   [9/9] init_version (0.0.1):
+```
+
+
+```sh
+go env -w GOSUMDB=off
+go mod tidy
+
+make build
+```
+
+
+## git-demo为例
+
+### 配置
+
+conifg.yaml 文件为项目的配置文件，默认内容如下：
+
+```yaml
+log:
+  level: "info" # 日志级别: debug, info, warn, error, fatal, panic
+  file: "" # 日志输出文件路径，留空则不输出到文件
+  caller: false # 是否在日志中包含调用者信息（文件和行号）
+  json: false # 是否以JSON格式输出日志
+```
+
+### 测试
+
+```sh
+$ ./bin/git-demo
+A longer description
+
+Usage:
+  git-demo [flags]
+  git-demo [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  logtest     Test logging functionality with different log levels
+  version     Print the version details
+
+Flags:
+  -c, --config string      yaml config file
+  -h, --help               help for git-demo
+      --log-caller         Include the caller file and line number
+      --log-file string    Write logs in json format to this file
+      --log-json           Log as json messages
+      --log-level string   Set the log level (debug, info, warn, error, fatal, panic) (default "info")
+
+Use "git-demo [command] --help" for more information about a command.
+
+
+
+# 查看版本
+$ ./bin/git-demo version
+Version: 0.0.1
+Build Date: 2026-01-09-14:32:16
+Git ref: a342433d9a7770cf63cee6a8cb3993e53e219311-DIRTY
+sha256: 287ac8eeee5908de92d2edfcb62256863f2d1039e834253b3bdd3f0fb7fa755d
+OS: linux
+Arch: amd64
+
+
+# 使用默认日志级别 (info)
+$ ./bin/git-demo logtest
+2:35PM INF This is an info message - shown when log level is info or lower
+2:35PM WRN This is a warning message - shown when log level is warn or lower
+2:35PM ERR This is an error message - shown when log level is error or lower
+
+
+# 设置日志级别为 debug (会显示所有日志)
+$ ./bin/git-demo logtest --log-level debug
+
+# 设置日志级别为 warn (只会显示 warn 和 error 级别的日志)
+$ ./bin/git-demo logtest --log-level warn
+
+# 使用配置文件
+$ ./bin/git-demo logtest -c bin/config.yml
+
+# 显示调用者信息
+$ ./bin/git-demo logtest -c bin/config.yml --log-caller
+
+# JSON格式日志
+$ ./bin/git-demo logtest --log-json
+
+# 日志文件输出
+$ ./bin/git-demo logtest -c bin/config.yml --log-file bin/log.log
+
+# 使用 JSON 格式输出日志
+$ ./bin/git-demo logtest --log-json
+
+# 结合其他日志选项
+$ ./bin/git-demo logtest --log-level debug --log-json --log-caller
+$ ./bin/git-demo logtest -c bin/config.yml --log-json --log-file bin/log.json
 ```
--- {{cookiecutter.app_name}}/cmd/root.go
+++ {{cookiecutter.app_name}}/cmd/root.go
@@ -18,16 +18,39 @@ var rootCmd = &cobra.Command{
 	Use:     "{{ cookiecutter.bin_name }}",
 	Short:   "A brief description of your application",
 	Long:    `A longer description`,
-	Version: fmt.Sprintf("%s", version.GetVersion()),
+	// Version: fmt.Sprintf("%s", version.GetVersion()),
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		logger.Init(viper.GetString("log.level"), viper.GetBool("log.caller"), viper.GetString("log.file"), viper.GetBool("log.json"))
 	},
 {% if cookiecutter.subcommands != "y" %}
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Info().Msg("Running root info")
-		log.Debug().Msg("Running root debug")
+		cmd.Help()
 	},
 {% endif %}
+}
+
+// subcommands version
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version details",
+	Long:  `Print the version details including build date, git ref, SHA256, OS and Arch.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		v := version.GetVersion()
+		fmt.Print(v.Details())
+	},
+}
+
+// subcommands logtest
+var logtestCmd = &cobra.Command{
+	Use:   "logtest",
+	Short: "Test logging functionality with different log levels",
+	Long:  `This command demonstrates logging with different levels (debug, info, warn, error, fatal).`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Debug().Msg("This is a debug message - only shown when log level is debug")
+		log.Info().Msg("This is an info message - shown when log level is info or lower")
+		log.Warn().Msg("This is a warning message - shown when log level is warn or lower")
+		log.Error().Msg("This is an error message - shown when log level is error or lower")
+	},
 }
 
 func Execute() {
@@ -43,7 +66,7 @@ func init() {
 	// Config
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "{{ cookiecutter.config_type }} config file")
 
-	// Logging
+	// Logging - make all log flags persistent so they're available to subcommands
 	rootCmd.PersistentFlags().String("log-level", "info", "Set the log level (debug, info, warn, error, fatal, panic)")
 	viper.BindPFlag("log.level", rootCmd.PersistentFlags().Lookup("log-level"))
 
@@ -53,6 +76,10 @@ func init() {
 	rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number")
 	viper.BindPFlag("log.caller", rootCmd.PersistentFlags().Lookup("log-caller"))
 
-	rootCmd.Flags().Bool("log-json", false, "Log as json messages")
-	viper.BindPFlag("log.json", rootCmd.Flags().Lookup("log-json"))
+	rootCmd.PersistentFlags().Bool("log-json", false, "Log as json messages")
+	viper.BindPFlag("log.json", rootCmd.PersistentFlags().Lookup("log-json"))
+
+	// subcommands
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(logtestCmd)
 }
